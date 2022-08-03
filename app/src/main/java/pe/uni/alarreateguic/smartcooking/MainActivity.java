7ebcbb7fa9f3d474a73b6917660ecd6a11b85fb6
Angel Larreategui Castro <44907747+MrAngelwwev2@users.noreply.github.com>
2022-08-02 23:33:20 -0500

    progress bar

--- app/src/main/java/pe/uni/alarreateguic/smartcooking/MainActivity.java
+++ app/src/main/java/pe/uni/alarreateguic/smartcooking/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         TimerTask tarea = new TimerTask() {
             @Override
