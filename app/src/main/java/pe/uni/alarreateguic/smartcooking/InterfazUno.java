a4482ffbbd6976b80f2dd5295f4e3652e12c7a67
Angel Larreategui Castro <44907747+MrAngelwwev2@users.noreply.github.com>
2022-08-04 13:37:49 -0500

    diseño

--- app/src/main/java/pe/uni/alarreateguic/smartcooking/InterfazUno.java
+++ app/src/main/java/pe/uni/alarreateguic/smartcooking/InterfazUno.java
@@ -1,14 +1,23 @@
 package pe.uni.alarreateguic.smartcooking;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InterfazUno extends AppCompatActivity {
+
+    TextView recibeNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interfaz_uno);
+
+        recibeNombre = findViewById(R.id.recibeNombre);
+
+        Bundle recibeDatos = getIntent().getExtras();
+        String nombre = recibeDatos.getString("keyDatos");
+
+        recibeNombre.setText("Hola " + nombre);
     }
 }
--- app/src/main/java/pe/uni/alarreateguic/smartcooking/PrincipalActivity.java
+++ app/src/main/java/pe/uni/alarreateguic/smartcooking/PrincipalActivity.java
@@ -2,13 +2,21 @@ package pe.uni.alarreateguic.smartcooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-    public void buttonPress(View view){
+    Button continuar;
+    EditText ingresaNombre;
 
+
+    public void buttonPress(View view){
+        Toast.makeText(PrincipalActivity.this, "Por favor, ingrese su nombre.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -16,6 +24,20 @@ public class PrincipalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        continuar = (Button)findViewById(R.id.botonContinuar);
+        ingresaNombre = findViewById(R.id.ingresaNombre);
+
+        continuar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Bundle enviaDatos = new Bundle();
+                enviaDatos.putString("keyDatos", ingresaNombre.getText().toString());
+                Intent intent = new Intent(PrincipalActivity.this,InterfazUno.class);
+                intent.putExtras(enviaDatos);
+                startActivity(intent);
+            }
+        });
 
     }
 }
